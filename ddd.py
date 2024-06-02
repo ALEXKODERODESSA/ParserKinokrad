48387789aa781a764c0667570e6f79110c029a29
ALEXKODERODESSA <olimpmadis@gmail.com>
2024-06-02 14:23:18 +0300

    Пробно выгружаю проект s_venv

---
+++ ddd.py
@@ -0,0 +1,55 @@
+I = 33
+
+
+def печать_четных_чисел(n):
+    print("Петька pyfrb от 0 до", n, ":")
+    print("Петька pyfrb от 0 до", n, ":")
+
+    sss = (s + f + d + uuu)
+    vines(sss)
+    vines(sss)
+    # Используем цикл while для печати четных чисел от n до 0
+    print("попса pyfrb от", n, "попса:")
+    print("попса pyfrb от", n, "попса:")
+    print("попса pyfrb от", n, "попса:")
+
+пися 
+    method_name()
+print("попса pyfrb от", n, "до 0:")
+пися
+s = 3
+sss= 0
+sadf = I
+sss= 0
+print("Петька pyfrb от", n, "до 0:")
+print("Петька pyfrb от", n, "до 0:")
+Четные pyfrb от", n, "до 0:")
+
+
+uuu= 77Четные pyfrb от", n, "до 0:")
+
+def method_name(s):
+    while n >= 0:
+        if n % 2 == 0:
+            d_t = s + f + d + uuu
+            print(n, end=" ")
+        n -= 1
+    print()  # Для перехода на новую строку
+
+
+def vines(n):
+    f = 33
+
+    # Используем цикл for для печати четных чисел от 0 до n
+    for i in range(n + 1):
+        a = 33
+        if i % 2 == 0:
+            print(i, end=" ")
+
+    t_ = ((s + f + d + uuu) / 2)
+    print(t_)  # Для перехода на новую строку
+
+
+# Пример использования функции
+печать_четных_чисел(10)
+
---
+++ sss.py
@@ -0,0 +1,28 @@
+asdd
+hasattr(dasd
+        asd    uu    uyuuy       i
+asd     o
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      xcvxvhj
+aaa= 77
